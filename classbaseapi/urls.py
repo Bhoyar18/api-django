f8e736babeda8b8a2cae58292c14c1b25270a264
Vaishnavi Bhoyar <vaishubhoyar02@gmail.com>
2024-08-10 16:22:53 +0530

    api

--- classbaseapi/urls.py
+++ classbaseapi/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('StudentList/',StudentList.as_view(),name='stu_list'),
-    path('stu_info/<int:pk>',StudentDetail.as_view(),name='stu_details'),
+    # path('stu_info/<int:pk>',StudentDetail.as_view(),name='stu_details'),
     # path('list/',list,name='list')
 ]
 
